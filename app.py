11852368f5cf334f19bdce707d8ec137ffdb14c0
Abhishek-1135 <25LBCS1135@culkomail.in>
2026-07-27 00:06:47 +0530

    Fix invalid import statement

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import 
+import pymupdf 
 import re
 from io import BytesIO
 
@@ -283,7 +283,7 @@ def extract_text_from_pdf(uploaded_file):
 
     pdf_bytes = uploaded_file.read()
 
-    document = .open(
+    document = pymupdf.open(
         stream=pdf_bytes,
         filetype="pdf"
     )
